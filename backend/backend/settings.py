717d375ea03a847bff88f40f05ba6371dc89aff1
Ivan <itanmare@gmail.com>
2025-04-26 23:26:14 +0300

    still trying to fix 502 error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,14 +175,17 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "https://next-pizza-production.up.railway.app",
-    "https://next-pizza-lilac-five.vercel.app"
-]
+#CORS_ALLOWED_ORIGINS = 
+CORS_ALLOW_ALL_ORIGINS = True
+
+#[
+#    "http://localhost:3000",
+#    "http://127.0.0.1:3000",
+#    "http://localhost:8000",
+#    "http://127.0.0.1:8000",
+#    "https://next-pizza-production.up.railway.app",
+#    "https://next-pizza-lilac-five.vercel.app"
+#]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
